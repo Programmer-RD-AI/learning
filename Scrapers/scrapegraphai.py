208bbb1b8d23a2d878e17827625526e382feb90b
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-02-04 18:24:32 +0530

    Remove Scrapers

--- Scrapers/scrapegraphai.py
+++
@@ -1,9 +0,0 @@
-from scrapegraph_py import Client
-
-client = Client(api_key="sgai-d1b0fe77-25ca-43a6-8541-da22c9a86761")
-
-response = client.markdownify(
-    website_url="https://blog.bytebytego.com/p/ep148-deepseek-1-pager"
-)
-
-print(response)
