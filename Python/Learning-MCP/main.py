376be2ae3c8130be80a08fe10ba58babee991b0d
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-04-06 00:05:15 +0530

    Learning-MCP

---
+++ Python/Learning-MCP/main.py
@@ -0,0 +1,19 @@
+from fastmcp import FastMCP
+
+# Create a named server
+mcp = FastMCP("My App")
+
+# Specify dependencies for deployment and development
+mcp = FastMCP("My App", dependencies=["pandas", "numpy"])
+
+
+@mcp.resource("config://app")
+def get_config() -> str:
+    """Static configuration data"""
+    return "App configuration here"
+
+
+@mcp.resource("users://{user_id}/profile")
+def get_user_profile(user_id: str) -> str:
+    """Dynamic user data"""
+    return f"Profile data for user {user_id}"
