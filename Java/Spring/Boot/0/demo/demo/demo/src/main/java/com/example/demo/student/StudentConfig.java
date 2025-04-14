47ae8020d93f6f8b4ead45050898cfc6d8d8874b
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-04-15 03:10:36 +0530

    Java/Spring/Boot/0/demo

--- Java/Spring/Boot/0/demo/demo/demo/src/main/java/com/example/demo/student/StudentConfig.java
+++ Java/Spring/Boot/0/demo/demo/demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,4 +1,25 @@
 package com.example.demo.student;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+@Configuration
 public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student test = new Student(
+                    "Test",
+                    21,
+                    LocalDate.of(2000, Month.JANUARY,12),
+                    "Test@gmail.com"
+            );
+
+            studentRepository.save(test);
+        };
+    }
 }
